ce08d8d03556106ea88219e080249c522306d9ee
Sean Curran <srcurran@gmail.com>
2019-06-07 15:16:59 -0400

    functional completion for launch

--- static/js/cubhouse.js
+++ static/js/cubhouse.js
@@ -1,9 +1,15 @@
 $(function(){
+	/* animate the dudes on scroll */
 	$(window).scroll(function(e){
+		/* adjustments for positioning and speed */
 		myScroll = $(window).scrollTop() * -1;
 		myScroll = myScroll + 440;
 		myScroll = myScroll * .25;
+		/* move the background image based on scroll position */
 		$("div.root section#hero-homepage").css("background-position", myScroll+"% 100%")
+	});
+	$(".scroll").click(function(event){
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 	});
 });
 
